a5ed89560d9ff80d863463e5d33f8f3e021220a3
Ben Weedon <ben@weedon.email>
2022-10-28 20:02:09 -0700

    Add run helper script

---
+++ run.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+param(
+    [ValidateSet('simpleinterp', 'opinterp')]
+    [Parameter(Mandatory, Position = 0)]
+    [String] $Bin,
+
+    [ValidateSet('mandelbrot', 'factor')]
+    [Parameter(Mandatory, Position = 1)]
+    [String] $SourceFile,
+
+    [Switch] $BuildDebug,
+
+    [Parameter(ValueFromPipeline)]
+    [String] $Pipeline = $null
+)
+
+$local:ErrorActionPreference = 'Stop'
+
+Push-Location $PSScriptRoot
+try {
+    $releaseArg
+    if ($BuildDebug) {
+        $releaseArg = ''
+    } else {
+        $releaseArg = '--release'
+    }
+
+    if ($Pipeline) {
+        $Pipeline | cargo run $releaseArg --bin $Bin -- corpus/$SourceFile.bf
+    } else {
+        cargo run $releaseArg --bin $Bin -- corpus/$SourceFile.bf
+    }
+} finally {
+    Pop-Location
+}
