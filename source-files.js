ab062ea8893dc835c2d6944fb5d29f525eb02bbc
binyomen <binyomen@users.noreply.github.com>
2022-10-29 01:38:56 +0000

    deploy: dd760bd09bad026f02167eabde1dfb2556e039cc

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var sourcesIndex = JSON.parse('{\
 "bench":["",[],["lib.rs"]],\
-"simpleinterp":["",[],["lib.rs"]],\
+"simpleinterp":["",[],["lib.rs","parser.rs","vm.rs"]],\
 "util":["",[],["error.rs","lib.rs","run.rs"]]\
 }');
 createSourceSidebar();
